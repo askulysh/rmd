2a3d1dbee7d607465b098b8a1c980513f8d5087d
Andriy Skulysh <askulysh@gmail.com>
2023-02-26 13:49:52 +0200

    allow variable heat width
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- csdr2heatmap.go
+++ csdr2heatmap.go
@@ -9,12 +9,12 @@ import (
 func main() {
     center := flag.Int("c", 144000000, "# center frequency")
     rate := flag.Int("r", 2048000, "# sample rate")
+    samples := flag.Int("n", 4096, "# sample count")
     start := flag.Int64("s", time.Now().Unix(), "# start time")
     flag.Parse()
     startFreq := *center - *rate/2
     endFreq :=  *center + *rate/2
-    samples := 4096
-    buf := make([]string, 0, samples)
+    buf := make([]string, 0, *samples)
     scanner := bufio.NewScanner(os.Stdin)
     scanner.Split(bufio.ScanWords)
     count := 0
@@ -23,13 +23,13 @@ func main() {
         count++
         value := scanner.Text()
         buf = append(buf, value)
-        if count%samples == 0 {
+        if count%*samples == 0 {
             fmt.Print(t.Format("2006-01-02, 15:04:05, "))
             t = t.Add(time.Second)
-            fmt.Printf("%d, %d, %d, %d, ", startFreq, endFreq, samples, samples)
+            fmt.Printf("%d, %d, %d, %d, ", startFreq, endFreq, *samples, *samples)
             for i := range buf {
                 fmt.Print(buf[i])
-                if i < samples-1 {
+                if i < *samples-1 {
                     fmt.Print(", ")
                 }
             }
